ccfedfd43a403c56418590005e96a3a875666f6c
Ahmad Nidzam Musthafha <54913708+nidzammst@users.noreply.github.com>
2024-05-22 14:16:32 +0000

    init nextjs app with shadcn-ui and clerk

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { ModeToggle } from "@/components/ModeToggle";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
+const page = () => {
+  return <div></div>;
+};
+
+export default page;
